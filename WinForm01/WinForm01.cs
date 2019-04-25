733eb2be656aab8989da3ce9da38619a9bb45dd2
Oleg <olegfurman34@gmail.com>
2019-04-25 13:11:22 +0300

    Виконано 2 завдання

---
+++ WinForm01/WinForm01.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForm01
+{
+    public partial class WinForm01 : Form
+    {
+        public WinForm01()
+        {
+            InitializeComponent();
+        }
+
+        private void WinForm01_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            double y;
+            Console.WriteLine("Введите  a ");
+            double a = Convert.ToDouble(textBox1.Text);
+            Console.WriteLine("Введите  b ");
+            double b = Convert.ToDouble(textBox2.Text);
+            Console.WriteLine("Введите  x ");
+            double x = Convert.ToDouble(textBox3.Text);
+            y = 2.4 * Math.Abs((Math.Pow(x, 2) + b) / a) + (a - b) * Math.Pow(Math.Sin(a - b), 2) + Math.Pow(10, -2) * (x - b);
+            textBox4.Text = y.ToString();
+        }
+    }
+}
